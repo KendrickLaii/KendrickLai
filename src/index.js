b0e08f26862acb8b7e4367d14b6bffd15efe1e6b
Kendrick <75213494+KendrickLaii@users.noreply.github.com>
2021-09-07 16:19:38 +0800

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -5,27 +5,9 @@ const scroll = new LocomotiveScroll({
     smartphone: {
         smooth: true,
         direction: 'horizontal',
+    },
+    tablet: {
+        smooth: true,
+        direction: 'horizontal',
     }
-});
-
-
-$(document).ready(function () {
-    
-    $('#to-home').on('click', () => {
-        scroll.scrollTo('#home')
-    });
-
-    $('#to-about').on('click', () => {
-        scroll.scrollTo('#about')
-    });
-
-    $('#to-works').on('click', () => {
-        scroll.scrollTo('#works')
-    });
-
-    $('#to-contact').on('click', () => {
-        scroll.scrollTo('#contact')
-    });
-
-    console.log('hello')
 });
