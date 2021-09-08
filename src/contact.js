2b6657dc8abb7ac4d944ab7afc44fd27d73505f9
Kendrick <75213494+KendrickLaii@users.noreply.github.com>
2021-09-08 18:09:29 +0800

    Update contact.js

--- src/contact.js
+++ src/contact.js
@@ -1,1 +1,14 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('[data-scroll-container]'),
+    smooth: true,
+    direction: 'vertical',
+    tablet: {
+        smooth: true,
+        direction: 'vertical',
+    },    
+});
+
+$(document).ready(() => {
+    $(".loading").fadeOut("slow");
+})
 
