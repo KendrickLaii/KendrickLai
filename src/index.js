c4886fe546a1c79c4cd88b3b3b6c3f32ecc70e76
Kendrick <75213494+KendrickLaii@users.noreply.github.com>
2021-12-04 00:12:47 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('[data-scroll-container]'),
-    smooth: true,
-    direction: 'vertical',
-    tablet: {
-        smooth: true,
-        direction: 'vertical',
-    },    
-});
+// Set the date we're counting down to
+var countDownDate = new Date("April 27, 2022 ").getTime();
 
-$(document).ready(() => {
-    $(".loading").delay(200).fadeOut("slow");
-    console.log('hello')
-})
+// Update the count down every 1 second
+var x = setInterval(function() {
 
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("countdown").innerHTML = days + "日 " + hours + "小時 "
+  + minutes + "分鐘 " + seconds + "秒 ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "EXPIRED";
+  }
+}, 1000);
 
 
