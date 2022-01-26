c307b5a2d65c9175951dcf7f46877790643985ca
ken <kendrick2k20@gmail.com>
2022-01-26 23:40:07 +0800

    text

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ var x = setInterval(function() {
   // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("countdown").innerHTML = "EXPIRED";
+    document.getElementById("countdown").innerHTML = "出得黎~!";
   }
 }, 1000);
 
