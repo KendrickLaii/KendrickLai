f4edd2bd9c031b271fb8af935b07349599b56618
ken <kendrick2k20@gmail.com>
2022-01-26 23:34:09 +0800

    change date

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("April 27, 2022 ").getTime();
+var countDownDate = new Date("April 22, 2022 ").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
